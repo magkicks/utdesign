d13525f8a02adb48aa5415ce8d088a7fa50b6d96
themagkicks <themagkicks@example.com>
2024-12-16 01:36:12 -0600

    Added background image and updated static file references

--- utdesign_backend/settings.py
+++ utdesign_backend/settings.py
@@ -192,3 +192,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #ALLOWED_HOSTS = ['capstone2024.pythonanywhere.com']
 #ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
